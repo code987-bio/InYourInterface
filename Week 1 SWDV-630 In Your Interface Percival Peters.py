3137f901b9b9f0e98926e0dc02a3d74b8b57a074
code987-bio <61991534+code987-bio@users.noreply.github.com>
2020-07-05 18:40:23 -0500

    Add files via upload

---
+++ Week 1 SWDV-630 In Your Interface Percival Peters.py
@@ -0,0 +1,13 @@
+class Teams:
+    def __init__(self, members):
+        self.__myTeam = members
+        
+    
+    def __len__(self):
+        return len(self.__myTeam)
+    
+def main():
+    classmates = Teams(['John','Steve','Tim'])
+    print(len(classmates))
+    
+main()
